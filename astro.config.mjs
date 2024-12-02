ea315d794f411821d09bc60f0b20d8f8b1a62b7d
burns.px <burns.px@digital-engine.com>
2024-12-02 16:36:13 +0800

    feat: install tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://pxxx666-learning-as-blog.deno.dev/'
+  site: 'https://pxxx666-learning-as-blog.deno.dev/',
+  integrations: [tailwind()]
 });
