95a42d65a73d841622e67fd48a00b67971e5f26a
burns.px <burns.px@digital-engine.com>
2024-12-02 15:51:00 +0800

    feat: learning to five sub two

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   site: 'https://pxxx666-learning-as-blog.deno.dev/'
+});
